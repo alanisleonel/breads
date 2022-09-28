6e32af510aa233924f85c502da49fdebea98ec64
Leonel Alanis <l.alanis1989@gmail.com>
2022-09-27 21:46:28 -0500

    sending index and show data

--- server.js
+++ server.js
@@ -5,14 +5,19 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
-// Routes
-app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads!')
-})
+// Middleware
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads!')
+})
 
 // Listen
 app.listen(PORT, () => {
