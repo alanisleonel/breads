b60a5eef79c25eea31e5223452e7dcd4fd0c5716
Leonel Alanis <l.alanis1989@gmail.com>
2022-10-02 13:49:17 -0500

    edit and update routes completed

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -18,6 +18,14 @@ breads.get('/new', (req, res) => {
     res.render('new')
 })
 
+// Edit
+breads.get('/:indexArray/edit', (req, res) => {
+    res.render('edit', {
+        bread: Bread[req.params.indexArray],
+        index: req.params.indexArray
+    })
+})
+
 // Show
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
@@ -49,4 +57,16 @@ breads.delete('/:indexArray', (req, res) => {
     Bread.slice(req.params.indexArray, 1)
     res.status(303).redirect('/breads')
 })
+
+// Update
+breads.put('/:arrayIndex', (req, res) => {
+    if(req.body.hasGluten === 'on') {
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+    Bread[req.params.arrayIndex] = req.body
+    res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
 module.exports = breads
