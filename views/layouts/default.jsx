3f880f952e50065b2aa7d57881dbcafafd4912c3
Leonel Alanis <l.alanis1989@gmail.com>
2022-10-02 10:52:32 -0500

    Added a favicon

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -10,6 +10,8 @@ function Default(html) {
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css" 
                 integrity="sha512-EZLkOqwILORob+p0BXZc+Vm3RgJBOe1Iq/0fiI7r/wJgzOFZMlsqTa29UEl6v6U6gsV4uIpsNZoV32YZqrCRCQ==" crossOrigin="anonymous" />
                 <link rel="stylesheet" href="/main.css" />
+                {/* favicon */}
+                <link rel="icon" type="image/x-icon" href="/images/favicon.ico"/>
             </head>
             <body>
                 <div className="wrapper">
